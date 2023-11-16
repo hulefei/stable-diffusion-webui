a869ef63122c8a2c3ecefd9e25dc8d4c84828fcc
hulefei <hulefei29@gmail.com>
2023-11-16 19:19:04 +0800

    [add]添加 dockerfile

--- download/test.sh
+++ download/test.sh
@@ -5,5 +5,5 @@ echo "Hello, World!"
 echo "Downloading, this might take a while..."
 aria2c -x 10 --disable-ipv6 --input-file /download/links.txt --dir /models --continue
 
-# echo "Checking SHAs..."
-# parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
+echo "Checking SHAs..."
+parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
